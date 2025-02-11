15b8ab0d84792293bfc858455fa0b44b1d1946d4
Maksim Akhmetshin <maks.akhm@gmail.com>
2025-02-10 22:26:10 -0500

    Update decorators.py

--- tests/decorators.py
+++ tests/decorators.py
@@ -18,3 +18,20 @@ def check_raw_table(func):
         print("Raw table creation successfull")
         return result
     return wrapper
+
+#Data Pipeline	EVENT HUB	 	 	Testing framework
+ 	 	 	launch strategy	Trigger through fabric schedule
+ 	 	 	 	place the files in test path
+file gets dropped at test path	 	 	 	 
+file dropped event has been created	 	 	 	 
+correlation ID	 	get corelation ID based on file path in the event hub	 	 
+file path	 	 	 	 
+Azure funtion initiatead with	 	 	 	 
+correlation ID	 	 	 	 
+file path	azure file movement event	 	 	 
+config.json	 	get event for the corelation ID	 	run the  tests for the azure funtions
+ 	 	 	 	 
+raw table load	 	 	 	 
+correlation ID	publish event with raw table load	 	 	 
+file path	 	get event for the corelation ID	 	 
+ 	 	 	 	run the tests for the raw table load
